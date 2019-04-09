11d2309d1b30de004ce1b0b6bb3d12d60c7c36a6
Gonzalo Vilela Guerrero <gvilelaguerrero@hotmail.com>
2019-04-09 20:26:31 +0200

    prueba

---
+++ module-1/lab-mysql/your-code/seeding.sql
@@ -0,0 +1,8 @@
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00001', 'Petey Cruiser', 'Madrid');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00002', 'Anna Sthesia', 'Barcelona');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00003', 'Paul Molive', 'Berlin');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00004', 'Gail Forcewind', 'Paris');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00005', 'Paige Turner', 'Mimia');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00006', 'Bob Frapples', 'Mexico City');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00007', 'Walter Melon', 'Amsterdam');
+INSERT INTO `mydb`.`SALESPERSON` (`STAFF ID`, `NAME`, `STORE`) VALUES ('00008', 'Shonda Leer', 'São Paulo');
