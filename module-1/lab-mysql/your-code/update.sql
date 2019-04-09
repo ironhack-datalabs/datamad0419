ea1477d89110445972df6ef23365ec2f444d67fc
vicentecastillo <castillocasarejos.vicentec@gmail.com>
2019-04-09 21:16:51 +0200

    Documentos para el lab

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,6 @@
+Select* from lab_mysql.Coche;
+
+UPDATE `lab_mysql`.`Comprador` SET `Name`='PICASSO', `email`='ppicasso@gmail.com' WHERE `idComprador`='1';
+UPDATE `lab_mysql`.`Comprador` SET `Name`='LINCON', `email`='lincoln@us.gov' WHERE `idComprador`='2';
+UPDATE `lab_mysql`.`Comprador` SET `Name`='NAPOLEON', `email`='hello@napoleon.me' WHERE `idComprador`='3';
+UPDATE `lab_mysql`.`Coche` SET `VIN`='DAM41UDN3CHU2WVF6' WHERE `idCoche`='3';
