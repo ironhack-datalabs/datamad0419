34a41ed322dafab5e59f383864490621b7456d58
Miguel López <miguel.mish@gmail.com>
2019-04-09 20:03:28 +0200

    con update y delete

---
+++ module-1/lab-mysql/your-code/delete.sql
@@ -0,0 +1,1 @@
+DELETE FROM `cardealdb`.`Car` WHERE (`idCar` = '6');
---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,5 @@
+UPDATE `cardealdb`.`SalesPerson` SET `store` = 'Miami' WHERE (`idSalesPerson` = '5') and (`SalesPersonID` = '00005');
+
+UPDATE `cardealdb`.`Customer` SET `email` = 'ppicasso@gmail.com' WHERE (`idCustomer` = '1') and (`CustomerID` = '10001');
+UPDATE `cardealdb`.`Customer` SET `email` = 'lincoln@us.gov' WHERE (`idCustomer` = '2') and (`CustomerID` = '20001');
+UPDATE `cardealdb`.`Customer` SET `email` = 'hello@napoleon.me' WHERE (`idCustomer` = '3') and (`CustomerID` = '30001');
