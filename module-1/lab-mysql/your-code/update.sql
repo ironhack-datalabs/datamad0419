08d37303aec135392ba2b517c2879cd6ed2282c5
YonatanRA <yeho@mail.com>
2019-04-09 19:46:56 +0200

    sql

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,13 @@
+
+
+
+UPDATE SalesPerson SET city= 'Miami' WHERE staff_id = 00005;
+
+
+
+
+
+UPDATE `mydb`.`customers` SET `mail`='ppicasso@gmail.com' WHERE `idcustomers`='10001';
+UPDATE `mydb`.`customers` SET `mail`='lincoln@us.gov' WHERE `idcustomers`='20001';
+UPDATE `mydb`.`customers` SET `mail`='te 	hello@napoleon.me' WHERE `idcustomers`='30001';
+
