c699844477bcc6ab8486894786f11f0839e0ae17
Dani Alonso <alonsopdani@gmail.com>
2019-04-10 18:52:07 +0200

    archivo solutions.sql añadido

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,48 @@
+-- challenge 1
+
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', titles.title AS 'TITLE', publishers.pub_name AS 'PUBLISHER'
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers ON publishers.pub_id = titles.pub_id;
+
+-- challenge 2
+
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', publishers.pub_name AS 'PUBLISHER', COUNT(titles.title_id) AS 'TITLE COUNT'
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers ON publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id, publishers.pub_id
+ORDER BY COUNT(titles.title_id) DESC;
+
+-- challenge 3
+
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', SUM(sales.qty) as 'TOTAL'
+FROM authors
+INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN sales ON sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC
+LIMIT 3;
+
+-- challenge 4
+
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', IFNULL(SUM(sales.qty), 0) as 'TOTAL'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+LEFT JOIN sales ON sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC;
+
+-- Bonus
+
+SELECT authors.au_id as 'AUTHOR ID', authors.au_lname as 'LAST NAME', authors.au_fname as 'FIRST NAME', ifnull(sum((ifnull(titles.advance, 0)) + ((titles.price*ifnull(sales.qty, 0))*(titles.royalty/100)*(titleauthor.royaltyper/100))), 0) as 'PROFIT'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titles.title_id = titleauthor.title_id
+LEFT JOIN sales ON sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY PROFIT DESC
