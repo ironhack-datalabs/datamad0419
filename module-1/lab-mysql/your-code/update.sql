d01d3007229775126cb5b413591976d45be469be
Jacobo Pintos <pvjacobo@hotmail.com>
2019-04-10 09:09:00 +0200

    Lab MySQL solutions

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,3 @@
+UPDATE `jacobo`.`Customers` SET `Email` = 'lincoln@us.gov\n', `City` = 'Miami' WHERE (`CustomerID` = '20001');
+UPDATE `jacobo`.`Customers` SET `Email` = 'ppicasso@gmail.com' WHERE (`CustomerID` = '10001');
+UPDATE `jacobo`.`Customers` SET `Email` = 'hello@napoleon.me\n' WHERE (`CustomerID` = '30001');
