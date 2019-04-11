c61d32a4b4d84f286a62914cd56803247ce414a7
Carlos Dueñas <carlosdss84@gmail.com>
2019-04-11 10:42:29 +0200

    bug fixed: bonus limited to 3 results

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -13,7 +13,7 @@ FROM titleauthor INNER JOIN authors ON titleauthor.au_id = authors.au_id
 INNER JOIN titles ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 GROUP BY authors.au_id, publishers.pub_name
-ORDER BY authors.au_id DESC
+ORDER BY authors.au_id DESC, PUBLISHER DESC;
 
 -- CHALLENGE 3
 SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME',
@@ -44,4 +44,4 @@ LEFT JOIN titles
 ON titleauthor.title_id = titles.title_id
 GROUP BY authors.au_id
 ORDER BY PROFIT DESC
-LIMIT 23;
+LIMIT 3;
