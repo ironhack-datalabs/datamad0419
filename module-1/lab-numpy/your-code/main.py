fbc62dd966fb50129192e5f277261dda2d861e29
vicenteccastillo <castillocasarejos.vicentec@gmail.com>
2019-04-12 18:47:24 +0200

    Ejercicio numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -4,65 +4,73 @@ import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-numpy --version
+np.version.version
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+a = np.random.random((2,3,5))
 
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
+b= np.ones((5,2,3))
 
 
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+print(a.size)
+print(b.size)
+if (a.size) == b.size:
+    print("son iguales")
+else:
+    print ("son distintos")
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+No, porque la forma de las tablas es distinta 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c =np.transpose(b,(1,2,0))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d= a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print("la suma de los valores de a ( " + str(np.sum(a)) + " )es distinta a la suma de los valores de d (" + str(np.sum(d)) +")")
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e= a*c
 
 #13. Does e equal to a? Why or why not?
-
+a == e
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max= d.max()
+d_min= d.min()
+d_mean= d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty(2,3,5)
 
 
 """
@@ -75,10 +83,27 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for x in range(len(d)):
+    for y in range(len(d[x])):
+        for z in range(len(d[x][y])):
+            if d[x][y][z] == d_min:
+                   d[x][y][z] = 0
+            elif d_min < d[x][y][z] < d_mean:
+                   d[x][y][z] = 25
+            elif d[x][y][z] == d_mean:
+                   d[x][y][z] = 50
+            elif d[x][y][z] < d_max:
+                   d[x][y][z] = 75
+            else:
+                   d[x][y][z]= 100
 
 
+
+print(d)
+print(f)
 
 """
+
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
