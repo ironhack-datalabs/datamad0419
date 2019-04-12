290907222e5240427bc1c154f5fe23c7339b6cef
Miguel López <miguel.mish@gmail.com>
2019-04-12 15:40:54 +0200

    hasta transpose

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,41 +1,42 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print(np.version.version)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
+a= np.random.random((2,3,5))
+aa= np.ones((2,3,5))
+aaa= np.zeros((2,3,5))
 
 #4. Print a.
-
-
+print(a)
+print(aa)
+print(aaa)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b= np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+print(a.size)
+print(b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
+#np.add(ints
+#operandintsuld not be broadcast together with shapes (2,3,5) (5,2,3)
 
-
-
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+#9. Tranintse b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c= b.T
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
