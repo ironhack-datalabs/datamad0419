fb4c6f9b9bf0c61a9160fee3869e71269d13fa9d
Jacobo Pintos <pvjacobo@hotmail.com>
2019-04-12 19:03:27 +0200

    Entregable sin bonus

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,69 +1,80 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2,3,5))
 
+"""Looking into the Numpy documentation (https://docs.scipy.org/doc/numpy/reference/routines.random.html) 
+there are a explanation of the other ways that are:
+random_sample as np.random.random_sample,
+ranf as np.random.ranf and
+sample as numpy.random.sample"""
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size)
+print(b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+"""I cannot add a and b due to the different shapes of both arrays"""
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c=b.transpose(1,2,0)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d=np.add(a, c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
+print(d)
 
-
+"""d is one number bigger in each element than a. 
+This is because we added one value (c array compounded by ones) per each a element in step #10"""
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e=a*c
 
 #13. Does e equal to a? Why or why not?
 
-
-
+a==e
+"""e is equal to a. 
+That happens because array c is an array compouned by ones"""
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty((2,3,5))
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -75,8 +86,19 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
-
+for o in range(len(d)):
+    for p in range(len(d)):
+        for q in range(len(d)):
+            if d[o][p][q]<d_mean and d[o][p][q]>d_min:
+                f[o][p][q]=25
+            elif d[o][p][q]>d_mean and d[o][p][q]<d_max:
+                f[o][p][q]=50
+            elif d[o][p][q]==d_mean:
+                f[o][p][q]=75
+            elif d[o][p][q]==d_min:
+                f[o][p][q]=0
+            elif d[o][p][q]==d_min:
+                f[o][p][q]=100
 
 """
 #17. Print d and f. Do you have your expected f?
@@ -99,6 +121,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print(d)
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
