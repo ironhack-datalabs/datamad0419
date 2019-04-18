f651f7abdc38304c32dfba67a7192761215c37ec
YonatanRA <yeho@mail.com>
2019-04-18 21:01:13 +0200

    pandas-project

---
+++ module-1/pandas-project/your-code/shark_attack.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# shark_attack.py
+# 
+
+
+
+
+
+import pandas as pd                    # dataframe
+import numpy as np                     # numerical python
+
+
+
+
+
+
+
+datos=pd.read_csv('attacks.csv', encoding='ISO-8859-1')           # se crea el dataframe
+print (datos)
+
+
+
+
