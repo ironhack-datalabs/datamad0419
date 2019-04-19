d42d8931b16bbfb4f038246e6f194a6c8e0e53dc
YonatanRA <yeho@mail.com>
2019-04-19 13:13:34 +0200

    pandas-project

--- module-1/pandas-project/your-code/shark_attack.py
+++ module-1/pandas-project/your-code/shark_attack.py
@@ -10,8 +10,8 @@ sino tambien que especie provoca mayores daños no mortales.
 
 En una segunda parte intentare construir una serie temporal con esa informacion 
 para tratar de responder una pregunta:
-¿los tiburones se estan volviendo mas agresivos con el tiempo?   
-De ser cierto, ¿esta relacionado con la actividad humana?
+¿los tiburones se estan volviendo mas agresivos con el tiempo?.   
+De ser cierto, ¿esta relacionado con la actividad humana?.
 '''
 
 
@@ -29,27 +29,27 @@ datos=pd.read_csv('attacks.csv', encoding='ISO-8859-1')           # se crea el d
 #print (datos.columns)
 
 
-datos=datos[['Date', 'Type', 'Activity', 'Species ', 'Injury', 'Fatal (Y/N)']]  # se escogen los datos a tratar
-print (datos.head())
+datos=datos[['Year',  'Type', 'Activity', 'Species ', 'Injury', 'Fatal (Y/N)']]  # se escogen los datos a tratar
+# elimino el resto de columnas porque no me hacen falta para el proposito del estudio
+
+datos['Year']=datos[datos['Year']>=1940]['Year']              # una primera seleccion de datos, desde 1940
+datos=datos.dropna(how='all')                                 # elimina Nan, desde el registro 6304 no hay datos
+datos=datos.iloc[0:4782, :]                                   # ahora solo me quedo con los registros desde 1940, 4783 registros
+#print (datos['Year'])
+
+#print (datos.shape)
+datos=datos.dropna(how='all')                                 # elimina Nan, desde el registro 6304 no hay datos
+#print (datos.shape)
+null=datos.isna().sum()                                       # se miran los valores nulos
+#print (null[null>0])
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+datos=datos.fillna('UNKNOWN')
+#print (datos.shape)
+print (null[null>0])
+#print (datos['Species '])
+#print (datos)
+#print (datos.dtypes)
 
 
 
