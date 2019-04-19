061891a0712e58950dab53b2341180ef21485732
YonatanRA <yeho@mail.com>
2019-04-19 13:21:44 +0200

    pandas-project

--- module-1/pandas-project/your-code/shark_attack.py
+++ module-1/pandas-project/your-code/shark_attack.py
@@ -15,7 +15,7 @@ De ser cierto, ¿esta relacionado con la actividad humana?.
 '''
 
 
-
+#1º
 import pandas as pd                    # dataframe
 from matplotlib import pyplot as plt   # para plots
 
@@ -23,23 +23,31 @@ from matplotlib import pyplot as plt   # para plots
 
 
 
-
+#2º
 datos=pd.read_csv('attacks.csv', encoding='ISO-8859-1')           # se crea el dataframe
 #print (datos)
 #print (datos.columns)
 
 
+
+#3º
 datos=datos[['Year',  'Type', 'Activity', 'Species ', 'Injury', 'Fatal (Y/N)']]  # se escogen los datos a tratar
 # elimino el resto de columnas porque no me hacen falta para el proposito del estudio
+datos=datos.rename(columns={'Year': 'Año', 'Type': 'Tipo', 'Activity': 'Actividad',\
+                            'Species ': 'Especie', 'Injury': 'Daños', 'Fatal (Y/N)': 'Fatal'}) 
+# renombro las columnas por comodidad
 
-datos['Year']=datos[datos['Year']>=1940]['Year']              # una primera seleccion de datos, desde 1940
-datos=datos.dropna(how='all')                                 # elimina Nan, desde el registro 6304 no hay datos
-datos=datos.iloc[0:4782, :]                                   # ahora solo me quedo con los registros desde 1940, 4783 registros
-#print (datos['Year'])
 
-#print (datos.shape)
-datos=datos.dropna(how='all')                                 # elimina Nan, desde el registro 6304 no hay datos
-#print (datos.shape)
+
+#4º
+datos['Año']=datos[datos['Año']>=1940]['Año']     # una primera seleccion de datos, desde 1940
+datos=datos.dropna(how='all')                        # elimina Nan, desde el registro 6304 no hay datos
+datos=datos.iloc[0:4782, :]                          # ahora solo me quedo con los registros desde 1940, 4783 registros
+# print (datos['Año'])
+
+
+
+
 null=datos.isna().sum()                                       # se miran los valores nulos
 #print (null[null>0])
 
