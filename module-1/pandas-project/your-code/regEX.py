4ec4b766f6426122c3d6c8ef1462f2cb33de0a7f
YonatanRA <yeho@mail.com>
2019-04-19 18:28:16 +0200

    pandas-project

--- module-1/pandas-project/your-code/regEX.py
+++ module-1/pandas-project/your-code/regEX.py
@@ -9,6 +9,6 @@ import re
 def regEX(datos):
 	R=[]
 	for e in datos:
-		n_dato=re.findall('no[t]? i[a-z]+', e)
+		n_dato=re.sub('no[t]? ?i[a-z]+,?\W?\w?[a-z ]+,?\W?[a-z ]+','0', e)
 		R.append(n_dato)
 	return R
