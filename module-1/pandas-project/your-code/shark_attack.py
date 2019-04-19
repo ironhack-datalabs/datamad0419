276791bd72b1dee368d1c0394f50edf22c4d3926
YonatanRA <yeho@mail.com>
2019-04-19 17:47:57 +0200

    pandas-project

--- module-1/pandas-project/your-code/shark_attack.py
+++ module-1/pandas-project/your-code/shark_attack.py
@@ -18,6 +18,7 @@ De ser cierto, ¿esta relacionado con la actividad humana?.
 #1º
 import pandas as pd                    # dataframe
 from matplotlib import pyplot as plt   # para plots
+import re                              # expresiones regulares
 
 
 
@@ -41,10 +42,16 @@ datos=datos.rename(columns={'Year': 'Año', 'Type': 'Tipo', 'Activity': 'Activid
 
 
 #4º
-datos['Año']=datos[datos['Año']>=1940]['Año']        # una primera seleccion de datos, desde 1940
-datos=datos.dropna(how='all')                        # elimina Nan, desde el registro 6304 no hay datos
-datos=datos.iloc[0:4782, :]                          # ahora solo me quedo con los registros desde 1940, 4783 registros
-# print (datos['Año'])
+datos['Año']=datos[datos['Año']>=1940]['Año']         # una primera seleccion de datos, desde 1940, solo para visualizacion[i]
+#print (datos['Año'])
+datos=datos.dropna(how='all')                         # elimina Nan, desde el registro 6304 no hay datos
+#print (datos.shape)
+#datos=datos.drop_duplicates()                         # se eliminan duplicados, 5950 registros (¡!)
+#print (datos.shape)
+datos=datos.iloc[0:4782, :]                           # ahora solo me quedo con los registros desde 1940, 4783 registros
+print (datos['Año'])
+#print (datos.shape)
+
 
 
 
@@ -53,15 +60,20 @@ datos=datos.iloc[0:4782, :]                          # ahora solo me quedo con l
 datos=datos.fillna('UNKNOWN')                                   # renombro los valores nulos...
 datos=datos[datos['Especie'].map(lambda x: str(x)!='UNKNOWN')]  # ... y los elimino segun ese nombre.
 datos.index=range(len(datos))                                   # reindexo el frame por si lo necesito en el futuro 
-#print (datos.shape)                                             # 2993 registros
+datos=datos.drop_duplicates() 
+print (datos.shape)                                             # 2981 registros
 null=datos.isna().sum()                                         # se miran los valores nulos
 #print (null[null>0])                                            # el frame esta limpio
 
 
 
 
-#6º
 
+
+#6º
+#strings=[datos['Daños'][i].lower() for i in range(len(datos))]
+#print(strings)	
+	
 
 
 
