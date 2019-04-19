9564696c3a261f31ec3e043bc2bbca0a76da3041
YonatanRA <yeho@mail.com>
2019-04-19 11:28:18 +0200

    project-pandas

--- module-1/pandas-project/your-code/shark_attack.py
+++ module-1/pandas-project/your-code/shark_attack.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 # shark_attack.py
-# 
 
 
+
+'''
+Mi objetivo es relacionar la mortalidad y el tipo de daños causados con la 
+especie de tiburon para descubrir, no solo que especie es la que genera mayor tasa de mortalidad, 
+sino tambien que especie provoca mayores daños no mortales.
+
+En una segunda parte intentare construir una serie temporal con esa informacion 
+para tratar de responder una pregunta:
+¿los tiburones se estan volviendo mas agresivos con el tiempo?   
+'''
 
 
 
 import pandas as pd                    # dataframe
-import numpy as np                     # numerical python
-
+from matplotlib import pyplot as plt   # para plots
 
 
 
@@ -16,8 +24,32 @@ import numpy as np                     # numerical python
 
 
 datos=pd.read_csv('attacks.csv', encoding='ISO-8859-1')           # se crea el dataframe
-print (datos)
-print (datos.columns)
+#print (datos)
+#print (datos.columns)
+
+
+datos=datos[['Date', 'Type', 'Activity', 'Species ', 'Injury', 'Fatal (Y/N)']]  # se escogen los datos a tratar
+print (datos.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
