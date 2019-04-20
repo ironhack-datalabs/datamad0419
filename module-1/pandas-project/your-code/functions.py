a5a22f809aede0621a00eddfc6e3736d23ca5e78
Dani Alonso <alonsopdani@gmail.com>
2019-04-20 19:59:32 +0200

    funcion duplicados hecha

--- module-1/pandas-project/your-code/functions.py
+++ module-1/pandas-project/your-code/functions.py
@@ -1,1 +1,12 @@
-# función para strip
+def duplicates(id_column):
+    new_id_column = []
+    while len(id_column) != len(set(id_column)):
+        for id in id_column:
+            if id not in new_id_column:
+                new_id_column.append(id)
+            else:
+                id = str(id) + "a"
+    return new_id_column
+
+
+print(duplicates([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]))
