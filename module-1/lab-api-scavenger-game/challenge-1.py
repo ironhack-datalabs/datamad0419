9ea6a8c387ae067276abd15754a9cb0132db89e4
Miguel López <miguel.mish@gmail.com>
2019-04-24 16:28:58 +0200

    ch1

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,2 +1,22 @@
 # enter your code below
-main = 'ironhack-datalabs/datamad0419'
+import requests
+import json
+
+url = 'https://api.github.com/'
+
+with open('key.env') as kf:
+  key=kf.read()
+
+response = requests.get(url+'repos/ironhack-datalabs/datamad0419/forks', auth=('Mihlos', key))
+results = response.json()
+
+# with open('json.txt', 'w') as jf:
+#   jf.write(response.text)
+lang_array=[]
+
+for i in range(len(results)):
+  lang_array.append(results[i]['language'])
+
+lang_set= set(lang_array)
+#print(lang_array)
+print(lang_set)
