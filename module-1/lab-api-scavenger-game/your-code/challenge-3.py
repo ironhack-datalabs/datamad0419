f6fd506b33619ab7bfac2910f12aa4f527b3afa1
YonatanRA <yeho@mail.com>
2019-04-24 22:37:33 +0200

    challenge-3

--- module-1/lab-api-scavenger-game/your-code/challenge-3.py
+++ module-1/lab-api-scavenger-game/your-code/challenge-3.py
@@ -8,10 +8,24 @@ import requests
 import json
 
 
+url='https://api.github.com/repos/ironhack-datalabs/scavenger/contents/'
 
-r=requests.get('https://api.github.com/repos/ironhack-datalabs/scavenger/contents/').json()  
+r=requests.get(url).json()  
+
+#print (r)
 
 
+names=[value for d in r for key,value in d.items() if key=='name']
 			
-print (r)
-	
+#print (names)
+
+scaven=[requests.get(url+names[i]).json()[0]['name'] for i in range(1, len(names))\
+        if requests.get(url+names[i]).json()[0]['name'].endswith('.scavengerhunt')]
+
+#print (scaven)	
+
+
+
+
+
+
