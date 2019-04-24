431aae7584b9929ade82d67003b32b66cbcd5ea0
YonatanRA <yeho@mail.com>
2019-04-24 16:57:49 +0200

    challenge-1

---
+++ module-1/lab-api-scavenger-game/your-code/challenge-1.py
@@ -0,0 +1,29 @@
+# enter your code below
+
+# challenge-1.py
+
+
+
+import requests
+import json
+import pandas as pd
+
+
+#forks=requests.get('https://api.github.com/repos/ironhack-datalabs/datamad0419/forks?client_id=xxxx&client_secret=yyyy')
+#print (forks.content)
+
+
+
+t=pd.read_json('output.json', 0)
+
+print (t)
+
+
+
+
+
+
+
+
+
+
