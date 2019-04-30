a64a26bc3d23a3ea402ce536ab837ff42d643c49
Rafa <rafaegurollin@gmail.com>
2019-04-30 18:08:29 +0200

    faltan últimos retoques

--- module-1/pipelines-project/your-code/pipelinemejorado.py
+++ module-1/pipelines-project/your-code/pipelinemejorado.py
@@ -7,9 +7,9 @@ def acquire():
     cars = pd.read_csv('./data.csv', sep=';')
     return cars
 
-def wrangle(df):
+def wrangle(cars):
     cars1 = cars.drop_duplicates()
-    cars1 = cars[['make', 'model', 'months_old', 'power','gear_type','fuel_type','kms','price']]
+    cars1 = cars1[['make', 'model', 'months_old', 'power','gear_type','fuel_type','kms','price']]
     cars1['old_years'] = cars1['months_old'].divide(12).round()
     cars1 = cars1[['make', 'model', 'power','gear_type','fuel_type','kms','old_years','price']]
     cars1['model'] = cars1['model'].str.replace('C-Elys�e', 'C-Elys')
@@ -20,19 +20,26 @@ def acquire2():
     results = response.json()
     return results
 
-def wrangle2(df):
+def wrangle2(results):
     datacars = pd.DataFrame(results)
     datacars.rename(columns={'nome': 'make'}, inplace=True)
     datacars['make'] = datacars['make'].str.replace('Alfa Romeo', 'Alfa')
     return datacars
  
-def analyze(df):
+def analyze(cars1, datacars):
     s1 = pd.merge(cars1,datacars,how='left', on=['make'])
     table = pd.pivot_table(s1, index=['make','codigo','model'], aggfunc={'model':[len],'power': np.mean, 'kms':np.mean, 'old_years':np.mean, 'price':np.mean}).round()
     return table
 
-def report(df):
+def report(table):
     table1 = table[['model','kms','power','old_years','price']]
-    return table1.head()
+    return table1
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    cars = acquire()
+    cars1 = wrangle(cars)
+    results = acquire2()
+    datacars = wrangle2(results)
+    table = analyze(cars1, datacars)
+    print(report(table))
+    
