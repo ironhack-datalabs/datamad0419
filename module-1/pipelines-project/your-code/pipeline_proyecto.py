e8bc30acf9dfd6c6077b672b54ec412c5cb5ff7c
Eduardo Leste <eduleste@gmail.com>
2019-04-30 18:04:10 +0200

    version estable, hay que refactorizar

--- module-1/pipelines-project/your-code/pipeline_proyecto.py
+++ module-1/pipelines-project/your-code/pipeline_proyecto.py
@@ -1,19 +1,22 @@
+from tkinter import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def acquire():
-    data = pd.read_csv('Suicidios.csv')
+
+def acquire(path):
+    data = pd.read_csv(path)
     df=pd.DataFrame(data)
     return df
 
 def wrangle(df):
-    df=df.rename(columns = {' gdp_for_year ($) ':'gdp_for_year'},inplace=True)
-    df=df['gdp_for_year']=df['gdp_for_year'].str.replace(',','')
-    df=df['gdp_for_year']=df.gdp_for_year.astype(int)
-    df=df.columns = df.columns.str.strip()
-    df=df.drop(['population','suicides/100k pop','country-year', 'HDI for year','gdp_per_capita ($)', 'generation'], axis=1, inplace=True)
+    df.rename(columns = {' gdp_for_year ($) ':'gdp_for_year'},inplace=True)
+    print(df.dtypes)
+    df['gdp_for_year']=df['gdp_for_year'].str.replace(',','',regex=False)
+    df['gdp_for_year']=df.gdp_for_year.astype(int)
+    df.columns = df.columns.str.strip()
+    df.drop(['population','suicides/100k pop','country-year', 'HDI for year','gdp_per_capita ($)', 'generation'], axis=1, inplace=True)
     df=df.query("country=='Spain' | country=='Germany' | country=='France' | country=='Italy' | country=='Belgium'| country=='Denmark'| country=='Croatia'| country=='Finland'| country=='Slovakia'| country=='Greece'| country=='Austria' | country=='Bulgary'| country=='Slovenia'| country=='Estonia' | country=='Hungary'| country=='Ireland' | country=='Poland'| country=='Portugal' | country=='Netherlands'| country=='Romania'| country=='Sweden'| country=='Czech Republic'")
     return df
 
@@ -23,23 +26,24 @@ def analyze(df, selected):
     df=df.loc[df.country==str(selected)]
     return df
 
-def visualize(df):
-    df=df.sort_values(by=['year', 'suicides_no'], inplace=True)
+def visualize(x):
+    df=x.sort_values(by=['year', 'suicides_no'], inplace=False)
     grafica_1=df.plot(x='year', y=['suicides_no'])
-    df=df.sort_values(by=['year', 'gdp_for_year'], inplace=True)
+    df=x.sort_values(by=['year', 'gdp_for_year'], inplace=False)
     grafica_2=df.plot(x='year', y=['gdp_for_year'])
     return grafica_1, grafica_2
 
 def save_viz(grafica_1, grafica_2): # TENGO DOS GRAFICAS
-    fig = barchart.get_figure(grafica_1)
+    fig = grafica_1.get_figure()
     fig.savefig('n_suicidios' + '.png')
-    fig = barchart.get_figure(grafica_2)
+    fig = grafica_2.get_figure()
     fig.savefig('ev_pib' + '.png')
 
 if __name__ == '__main__':
-    selected=input('Hi, we are an app that deals with suicide data within the EU. Please, enter a Country of the UE and we will give you the correlation between number of suicides and the evolution of the gdp')
+    selected= 'Spain' # input('Hi, we are an app that deals with suicide data within the EU. Please, enter a Country of the UE and we will give you the correlation between number of suicides and the evolution of the gdp')
+    filename = 'Suicidios.csv'
 
-    data = acquire('Suicidios.csv')
+    data = acquire(filename)
     filtered = wrangle(data)
     results = analyze(filtered, selected)
     grafica_1, grafica_2 = visualize(results)
