317eb32cc62e6025bab802ba36bf9d013cb28768
Jacobo Pintos <pvjacobo@hotmail.com>
2019-05-01 09:36:04 +0200

    v3

--- module-1/pipelines-project/your-code/main.py
+++ module-1/pipelines-project/your-code/main.py
@@ -8,14 +8,14 @@ import numpy as np
 from dotenv import load_dotenv
 from collections import Counter
 
-DataFrame='movie_metadata.csv'
-token=open(".env").read()
-
 from analysis import *
 
 if __name__=="__main__":
 
-    run = read(DataFrame)
+    csv_name='movie_metadata.csv'
+    token = 'BbhyEKhqsxJFzs52ERfm' #open('.env').read()
+
+    run = read(csv_name)
     CPI_index = API_input(token)
     imdb = set_up(run)
     imdb = data_cleaning(imdb)
@@ -23,14 +23,14 @@ if __name__=="__main__":
     imdb = data_binning(imdb)
     imdb = data_embellish(imdb)
     imdb = merge(imdb, CPI_index)
-    csv = toCSV(imdb)
-    plot1 = plot_genre(imdb)
-    plot2 = plot_bins(imdb)
-    plot3 = plot_yearVSmovies(imdb)
-    plot4 = plot_yearVSvotes(imdb)
-    plot5 = plot_top10budget(imdb)
-    plot6 = plot_worsttop10(imdb)
-    plot7 = plot_corr_coreVSbudget(imdb)
+    toCSV(imdb)
+    plot_genre(imdb)
+    plot_bins(imdb)
+    plot_yearVSmovies(imdb)
+    plot_yearVSvotes(imdb)
+    plot_top10budget(imdb)
+    plot_worsttop10(imdb)
+    plot_corr_coreVSbudget(imdb)
     output1 = categorizer(imdb)
     output2 = director(imdb)
     
